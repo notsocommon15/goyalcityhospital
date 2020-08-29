384794ef7698db98e4bbb961af086336754fe19c
notsocommon15 <kediasubham15@gmail.com>
2020-08-30 01:21:12 +0530

    first

---
+++ gallery.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Goyal City Hospital - Health Care &amp; Medical HTML5 Template</title>
+<meta name="keywords" content="HTML5,CSS3,HTML,Template,Multi-Purpose,M_Adnan,Corporate Theme,Goyal City Hospital,Health Care,Goyal City Hospital - Health Care & Medical HTML5 Template">
+<meta name="description" content="Goyal City Hospital - Health Care & Medical HTML5 Template">
+<meta name="author" content="M_Adnan">
+
+<!-- FONTS ONLINE -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,100,800,900,400,200,300' rel='stylesheet' type='text/css'>
+
+<!--MAIN STYLE-->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/ionicons.min.css" rel="stylesheet" type="text/css">
+<style>
+  #logo{
+    height:60px;
+    width:60px;
+  }
+</style>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+
+<!-- Page Wrap ===========================================-->
+<div id="wrap"> 
+  
+  <!-- TOP BAR ===========================================-->
+ <!-- <div class="top-bar light">
+    <div class="container">
+      <ul class="pull-left">
+        <li><a href="index-1.php">Welcome To Our Goyal City Hospital Center</a></li>
+      </ul>
+      
+      <!-- SOCIAL ICONS ===========================================-->
+     <!-- <ul class="social_icons">
+        <li class="facebook"><a href="#."><i class="fa fa-facebook"></i> </a></li>
+        <li class="twitter"><a href="#."><i class="fa fa-twitter"></i> </a></li>
+        <li class="dribbble"><a href="#."><i class="fa fa-dribbble"></i> </a></li>
+        <li class="instagram"><a href="#."><i class="fa fa-instagram"></i> </a></li>
+        <li class="googleplus"><a href="#."><i class="fa fa-google-plus"></i> </a></li>
+        <li class="pinterest"><a href="#."><i class="fa fa-pinterest"></i> </a></li>
+        <li class="linkedin"><a href="#."><i class="fa fa-linkedin"></i> </a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- HEADER ===========================================-->
+  <header>
+    <div class="container"> 
+      <!--======= LOGO =========-->
+      <div class="logo"> <a href="index-1.php"><img src="images/lo.png" id="logo" alt="" ></a> </div>
+      
+      <!--======= NAVIGATION =========-->
+      <nav class="webimenu"> 
+        <!-- MENU BUTTON RESPONSIVE -->
+        <div class="menu-toggle"> <i class="fa fa-bars"> </i> </div>
+        <ul class="ownmenu">
+          <li><a href="index-1.php">Home</a>
+            <!--<ul class="dropdown animated-3s fadeIn">
+              <li><a href="index-1.php">Home</a></li>
+              <li><a href="index-1.php">Home Slider</a></li>
+              <li><a href="index-2.php">Home 2</a></li>
+              <li><a href="index-header.php">header 1</a></li>
+            </ul>-->
+          </li>
+          <li><a href="02-about-us-1.php">About us </a>
+            <!--<ul class="dropdown animated-3s fadeIn">
+              <li><a href="02-about-us.php">About</a></li>
+              <li><a href="02-about-us-1.php">About 2</a></li>
+            </ul>-->
+          </li>
+          <li><a href="services.php">SERVICES </a>
+            <!--<ul class="dropdown animated-3s fadeIn">
+              <li><a href="services.php">SERVICES</a></li>
+              <li><a href="services-1.php">SERVICES 2</a></li>
+            </ul>-->
+          </li>
+          <li><a href="03-department.php">Department </a> </li>
+          <li class="active"><a href="gallery.php">Gallery </a>
+            <!--<ul class="dropdown animated-3s fadeIn">
+              <li><a href="gallery.php">GALLERY</a></li>
+              <li><a href="gallery-2-col.php">GALLERY 2 col</a></li>
+              <li><a href="gallery-3-col.php">GALLERY 3 col</a></li>
+              <li><a href="gallery-4-col.php">GALLERY 4 col</a></li>
+            </ul>-->
+          </li>
+          <li><a href="05-doctors-1.php">Doctors</a> 
+            <!--======= MEGA MENU =========-->
+            <!--<ul class="megamenu full-width animated-3s fadeIn">
+              <li class="row nav-post">
+                <div class="col-sm-6 boder-da-r">
+                  <ul>
+                    <li><a href="index-1.php">Home</a></li>
+                    <li><a href="index-1.php">Home Slider</a></li>
+                    <li><a href="index-2.php">Home 2</a></li>
+                    <li><a href="index-header.php">header 1</a></li>
+                    <li><a href="appointment.php">Appointment</a></li>
+                    <li><a href="02-about-us.php">About</a></li>
+                    <li><a href="02-about-us-1.php">About 2</a></li>
+                    <li><a href="services.php">SERVICES</a></li>
+                    <li><a href="services-1.php">SERVICES 2</a></li>
+                    <li><a href="03-department.php">Department</a></li>
+                    <li><a href="04-department-detail.php">Department Detail</a></li>
+                    <li><a href="team.php">TEAM</a></li>
+                    <li><a href="05-doctors.php">Doctors</a></li>
+                    <li><a href="05-doctors-1.php">Doctors 2</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-6">
+                  <ul>
+                    <li><a href="gallery.php">GALLERY</a></li>
+                    <li><a href="gallery-2-col.php">GALLERY 2 col</a></li>
+                    <li><a href="gallery-3-col.php">GALLERY 3 col</a></li>
+                    <li><a href="gallery-4-col.php">GALLERY 4 col</a></li>
+                    <li><a href="07-timetable.php">Timetable</a></li>
+                    <li><a href="08-blog.php">Blog</a></li>
+                    <li><a href="09-blog-single.php">Blog Single</a></li>
+                    <li><a href="10-shop.php">Shop</a></li>
+                    <li><a href="11-product-detail.php">Product Detail</a></li>
+                    <li><a href="12-contact.php">Contact</a></li>
+                    <li><a href="shortcodes.php">Shortcodes</a></li>
+                    <li><a href="coming-soon.php">Coming Soon</a></li>
+                    <li><a href="404.php">404</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>-->
+          </li>
+          <li><a href="12-contact.php">Contact</a> </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  
+  <!--======= CONTENT =========-->
+  <div class="content fix-nav-space"> 
+    
+    <!--======= SUB BANNER =========-->
+    <section class="sub-banner" data-stellar-background-ratio="0.5">
+      <div class="overlay">
+        <div class="container">
+          <h3>GALLERY</h3>
+          <p>Claritas est etiam processus dynamicus, qui sequ </p>
+
+          <!--======= Breadcrumbs =========-->
+          <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li class="active">GALLERY</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+    
+    <!--======= RECENT CASES =========-->
+    <section class="gallery-pages">
+      <div class="portfolio-wrapper">
+        <div class="container"> 
+          
+          <!--======= FILTERS =========-->
+          <ul class="filter">
+            <li><a class="active" href="#." data-filter="*">Show All</a></li>
+            <li><a href="#" data-filter=".dental">Dental</a></li>
+            <li><a href="#" data-filter=".cardiology">Cardiology</a></li>
+            <li><a href="#" data-filter=".disabled">For Disabled</a></li>
+            <li><a href="#" data-filter=".ophthalmology">Ophthalmology</a></li>
+            <li><a href="#" data-filter=".emergency">Emergency</a></li>
+          </ul>
+        </div>
+        <div class="container popup-gallery"> 
+          
+          <!--======= GALLERY ROW =========-->
+          <ul class="items gallery-item row">
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-4 item cardiology ophthalmology">
+              <section> <img class="img-responsive" src="images/img-1.jpg" alt="">
+                <div class="item-over"> <a href="images/img-1.jpg" class="item-in">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-4 item disabled emergency">
+              <section> <img class="img-responsive" src="images/img-2.jpg" alt="">
+                <div class="item-over"> <a href="images/img-2.jpg" class="item-in">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-4 item cardiology disabled ">
+              <section> <img class="img-responsive" src="images/img-3.jpg" alt="">
+                <div class="item-over"> <a href="images/img-3.jpg" class="item-in">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-8 item cardiology disabled ">
+              <section> <img class="img-responsive" src="images/img-4.jpg" alt="">
+                <div class="item-over"> <a href="images/img-4.jpg" class="item-in pa-5">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-4 item dental cardiology disabled ophthalmology">
+              <section> <img class="img-responsive" src="images/img-5.jpg" alt="">
+                <div class="item-over"> <a href="images/img-5.jpg" class="item-in pa-40">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-4 item disabled emergency">
+              <section> <img class="img-responsive" src="images/img-2.jpg" alt="">
+                <div class="item-over"> <a href="images/img-2.jpg" class="item-in">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+            
+            <!--======= GALLERY IMG 1 =========-->
+            <li class="col-sm-4 item  cardiology emergency ">
+              <section> <img class="img-responsive" src="images/img-3.jpg" alt="">
+                <div class="item-over"> <a href="images/img-3.jpg" class="item-in">
+                  <h5>Image Title Here</h5>
+                  <hr>
+                  <p>Aspernatur aut odit aut fugit, sed quia 
+                    consequuntur magni dolores.</p>
+                  </a> </div>
+              </section>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    
+    <!--======= Contact Info =========-->
+    <section class="contact-info">
+      <div class="container"> 
+        
+        <!--Address-->
+        <ul class="row">
+          <li class="col-md-4"> <i class="ion-ios-location-outline"></i>
+            <h5>Address</h5>
+            <p>Goyal City Hospital NH-2 Trans Yamuna Colony Firozabad Road ,Rambagh Agra 282006</p>
+          </li>
+          
+          <!--Hot line-->
+          <li class="col-md-4"> <i class="ion-iphone"></i>
+            <h5>Hotline</h5>
+            <p>0562 4001062</p>
+          </li>
+          
+          <!--Email Contact-->
+          <li class="col-md-4"> <i class="ion-ios-email-outline"></i>
+            <h5>Email contact</h5>
+            <p>info@goyalcityhospitals.com</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+  
+  <!--======= FOOTER =========-->
+  <footer>
+    <div class="container"> 
+      <!-- Row -->
+      <div class="row"> 
+        
+        <!-- Latest Tweet -->
+        <div class="col-md-3">
+          <div class="latest-tweet">
+            <h5>Latest tweets</h5>
+            <ul>
+              
+              <!--Tweet 1 -->
+              <li>
+                <p><span>@Goyal City Hospital</span> Sweets and Bakers 
+                  WordPress Theme' on @EnvatoMarket by themefore<a href="#."> http://t.co/we8Kf0</a></p>
+                <span class="date"> - Thursday April 9, 2015</span> </li>
+              
+              <!--Tweet 2 -->
+              <li>
+                <p><span>@Goyal City Hospital</span> In hendrerit in molestie consequat in <a href="#."> http://t.co/we8Kf0</a></p>
+                <span class="date"> - Thursday April 9, 2015</span> </li>
+              
+              <!--Tweet 3 -->
+              <li>
+                <p><span>@Goyal City Hospital</span> Duis autem vel eum iriure <span>@Goyal City Hospital</span> dolor in hendrerit in molestie consequat <a href="#."> http://t.co/we8Kf0</a></p>
+                <span class="date"> - Thursday April 9, 2015</span> </li>
+            </ul>
+          </div>
+        </div>
+        
+        <!-- About Us -->
+        <div class="col-md-6">
+          <div class="small-info"> <img src="images/lo.png" id="logo" alt="">
+            <p>We work in a friendly and efficient using the latest technologies and sharing our expertise to make a diagnosis and implement cutting-edge therapies.</p>
+            <ul class="social_icons">
+              <li class="facebook"><a href="#."><i class="fa fa-facebook"></i> </a></li>
+              <li class="twitter"><a href="#."><i class="fa fa-twitter"></i> </a></li>
+              <li class="linkedin"><a href="#."><i class="fa fa-linkedin"></i> </a></li>
+            </ul>
+            
+            <!-- News Letter -->
+            <h5>register newsletter</h5>
+            <form>
+              <input type="email" placeholder="Enter your email here" required>
+              <button type="submit"> Subscribe</button>
+            </form>
+          </div>
+        </div>
+        
+        <!-- Patient Guide -->
+        <div class="col-md-3">
+          <div class="links text-right">
+            <h5>Patient Guide</h5>
+            <ul>
+              <li><a href="#."> Choosing a doctor</a></li>
+              <li><a href="#."> Health journals</a></li>
+              <li><a href="#."> Insurance converage</a></li>
+              <li><a href="#."> Talking to your doctor</a></li>
+              <li><a href="#."> Goyal City Hospital error</a></li>
+            </ul>
+            
+            <!-- Timing -->
+            <div class="timing">
+              <h5>opening hours</h5>
+              <p>Mon to Fri <span> 8:00 am to 7:00pm</span></p>
+              <p>Sun & Sat <span>9:00 am to 5:00pm</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Rights -->
+    <div class="rights">
+      <p>© 2015 Goyal City Hospital. Made with by M_Adnan </p>
+    </div>
+  </footer>
+</div>
+<!-- Wrap End --> 
+<script src="js/jquery-1.11.0.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/own-menu.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/jquery.superslides.js"></script> 
+<script src="js/masonry.pkgd.min.js"></script> 
+<script src="js/jquery.stellar.min.js"></script> 
+<script src="js/jquery-ui-1.10.3.custom.js"></script> 
+<script src="js/jquery.magnific-popup.min.js"></script> 
+<script src="js/jquery.isotope.min.js"></script> 
+<script src="js/jquery.flexslider-min.js"></script> 
+<script src="js/appointment.js"></script> 
+<script src="js/jquery.downCount.js"></script>
+<script src="js/counter.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
